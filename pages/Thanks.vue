43e3df5d3f1aff04d2867c0ad0dfc8e15d87c2d7
koncrm <dev.mahmoud.adel@gmail.com>
2022-12-11 01:04:27 +0200

    Modify Thanks Page

--- pages/Thanks.vue
+++ pages/Thanks.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="flex items-center justify-center h-screen bg-white">
-    <div class="p-4 rounded shadow-lg ring ring-indigo-600/50">
+  <div class="flex items-center justify-center h-screen bg-black">
+    <div class="p-4 rounded shadow-lg ring ring-header">
       <div class="flex flex-col items-center space-y-2">
         <svg
           xmlns="http://www.w3.org/2000/svg"
-          class="text-green-600 w-28 h-28"
+          class="text-header w-28 h-28"
           fill="none"
           viewBox="0 0 24 24"
           stroke="currentColor"
@@ -16,10 +16,10 @@
             d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
           />
         </svg>
-        <h1 class="text-4xl font-bold">شكرا لك !</h1>
-        <p>سيتم متابعة الطلب معك فى أقرب وقت.</p>
+        <h1 class="text-4xl font-bold text-header">شكرا لك !</h1>
+        <p class="text-gray-200">سيتم متابعة الطلب معك فى أقرب وقت.</p>
         <a
-          class="inline-flex items-center px-4 py-2 text-white bg-indigo-600 border border-indigo-600 rounded rounded-full hover:bg-indigo-700 focus:outline-none focus:ring"
+          class="inline-flex items-center px-4 py-2 text-black bg-header border border-header rounded rounded-full hover:bg-black hover:text-header focus:outline-none focus:ring"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"

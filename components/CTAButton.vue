8ffb71c05bcb94fc77cf0ed43e9fe57277a9f829
koncrm <dev.mahmoud.adel@gmail.com>
2022-12-10 22:44:36 +0200

    Add Image Sizes and Format

--- components/CTAButton.vue
+++ components/CTAButton.vue
@@ -1,14 +1,14 @@
 <template>
   <div class="rounded-full">
     <div
-      class="w-full text-white font-extrabold text-xl md:text-3xl rounded-lg shadow-md focus:shadow-lg focus:outline-none active:shadow-lg transition duration-150 ease-in-out cursor-pointer"
+      class="text-white font-extrabold text-xl md:text-3xl rounded-lg shadow-md focus:shadow-lg focus:outline-none active:shadow-lg transition duration-150 ease-in-out cursor-pointer"
     >
       <a href="#form">
         <nuxt-img
           src="/img/section_6/bottom.png"
           alt="call_to_action"
           format="webp"
-          sizes="md:150vw sm:50vw"
+          sizes="md:300px sm:200px"
           style="pointer-events: none;"
           onContextMenu="return false;"
         />

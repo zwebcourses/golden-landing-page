4f2c210d972b352e58b56dc6aac95be9e0aeebf8
koncrm <dev.mahmoud.adel@gmail.com>
2022-12-11 19:12:50 +0200

    Modify Float Button

--- components/FloatBtn.vue
+++ components/FloatBtn.vue
@@ -4,7 +4,6 @@
     class="fixed z-90 bottom-10 right-8 bg-gradient-to-r from-header to-yellow-700 w-20 h-20 rounded-full drop-shadow-lg flex justify-center items-center text-black font-extrabold text-sm hover:bg-black hover:drop-shadow-2xl hover:animate-bounce duration-300"
     @click="$emit('openDialog')"
   >
-  تواصل <br>
-  معنا
+  تواصل معنا
   </button>
 </template>
